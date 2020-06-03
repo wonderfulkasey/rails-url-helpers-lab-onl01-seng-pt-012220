ff41002ea346c0dcaee8f30d690f5b62ce059bce
Kasey Arnold <kaseyarnold15@gmail.com>
2020-06-03 00:35:13 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,16 +1,35 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
-  end
+    @student = set_student
+        if @student.active == false
+          "This student is current inactive."
+        else "This student is currently active."
+        end
+      end
+
+      def activate
+        @student = set_student
+        if @student.active == false
+          @student.active = true
+          @student.save
+        else @student.active == true
+        end
+        redirect_to student_url
+      end
+
 
   private
 
     def set_student
       @student = Student.find(params[:id])
+      @student.active = !@student.active
+    @student.save
+    redirect_to student_path(@student)
     end
 end
